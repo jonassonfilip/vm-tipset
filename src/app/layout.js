0f307c0ef4e9deb8898c69f4cd418c0d294fbd36
Filip Jonasson <jonassonfilip@gmail.com>
2026-06-09 01:55:05 +0200

    Update fonts with Nunito

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import { Inter } from 'next/font/google';
+import { Nunito } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
+const nunito = Nunito({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'The huge World Cup bet',
@@ -12,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.ico" sizes="any" />
-      <body className={inter.className}>{children}</body>
+      <body className={nunito.className}>{children}</body>
     </html>
   );
 }
