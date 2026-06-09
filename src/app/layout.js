ca6e01ee33f98b19eef8e96402804b42261b16e3
Filip Jonasson <jonassonfilip@gmail.com>
2026-06-09 13:46:42 +0200

    Fixing fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,4 @@
-import { Inter } from 'next/font/google';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'The huge World Cup bet',
@@ -12,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.ico" sizes="any" />
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
