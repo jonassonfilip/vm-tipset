fd8fe0d8972513cd678f81b5692b11995724d1b2
Filip Jonasson <jonassonfilip@gmail.com>
2026-06-09 02:17:34 +0200

    adding futura

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,7 @@
 import { Inter } from 'next/font/google';
-import { Nunito } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
-const nunito = Nunito({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'The huge World Cup bet',
@@ -14,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.ico" sizes="any" />
-      <body className={nunito.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
